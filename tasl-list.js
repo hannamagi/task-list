d5869b26ce1b195fd0430576c21ef1ea19704f05
Hanna <hanna.magi.001@voco.ee>
2022-09-29 11:07:09 +0300

    As a user, I can add task [Delivers #183402498]

---
+++ tasl-list.js
@@ -0,0 +1,21 @@
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+
+form.addEventListener('submit', addTask)
+
+function addTask(e){
+    // create list item
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(taskInput.value))
+    li.className = 'collection-item'
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
+    a.setAttribute('href', '#')
+    li.appendChild(a)
+    // add to list
+    const ul = document.querySelector('ul')
+    ul.appendChild(li)
+    taskInput.value = ''
+    e.preventDefault()
+}
