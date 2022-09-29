d7aa31096f668539bf773fff26f740b6a94e7b19
Hanna <hanna.magi.001@voco.ee>
2022-09-29 11:18:39 +0300

    As a user I can delete a single task [Delivers ]

--- tasl-list.js
+++ tasl-list.js
@@ -19,3 +19,34 @@ function addTask(e){
     taskInput.value = ''
     e.preventDefault()
 }
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('ul')
+
+form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+
+function addTask(e){
+    // create list item
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(taskInput.value))
+    li.className = 'collection-item'
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
+    a.setAttribute('href', '#')
+    li.appendChild(a)
+    // add to list
+    const ul = document.querySelector('ul')
+    ul.appendChild(li)
+    taskInput.value = ''
+    e.preventDefault()
+}
+
+function deleteTask(e){
+    if(e.target.textContent == 'X'){
+        if(confirm('Are you sure to delete this task?')){
+            e.target.parentElement.remove()
+        }
+    }
+}
