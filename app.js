6ca56ec31d505fec88db5a80f1b5f3ea1b38e008
Hanna <hanna.magi.001@voco.ee>
2022-10-05 10:51:24 +0300

    As a user I can reload  page and see actual data [Delivers #183440375]

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const form  = document.querySelector('form')
 const taskInput = document.querySelector('#task')
 const taskList = document.querySelector('ul')
@@ -6,6 +7,7 @@ const delAllTasks = document.querySelector('#del-tasks')
 form.addEventListener('submit', addTask)
 taskList.addEventListener('click', deleteTask)
 delAllTasks.addEventListener('click', deleteAllTasks)
+document.addEventListener('DOMContentLoaded', getTasks)
 
 function addTask(e){
     // create list item
@@ -20,6 +22,7 @@ function addTask(e){
     // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
+    addTaskLS(taskInput.value)
     taskInput.value = ''
     e.preventDefault()
 }
@@ -28,6 +31,7 @@ function deleteTask(e){
     if(e.target.textContent == 'X'){
         if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.remove()
+            deleteTaskLS(e.target.parentElement.textContent.slice(0,-1))
         }
     }
 }
@@ -37,4 +41,53 @@ function deleteAllTasks(e){
     while (taskList.firstChild){
         taskList.removeChild(taskList.firstChild)
     }
+    localStorage.removeItem('tasks')
+}
+
+function addTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function deleteTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((taskLS, taskIndex) => {
+        if(taskLS === task){
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function getTasks(){
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((task) => {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(task))
+        li.className = 'collection-item'
+        const a = document.createElement('a')
+        a.appendChild(document.createTextNode('X'))
+        a.className = 'blue-text text-darken-2 secondary-content'
+        a.setAttribute('href', '#')
+        li.appendChild(a)
+        // add to list
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+    })
 }
